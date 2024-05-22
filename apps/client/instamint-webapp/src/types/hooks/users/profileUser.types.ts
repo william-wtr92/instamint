d4bcc9d474a43a0eef44cb67b9ecb1b067e9d75f
Thomas De Oliveira <deoliveirathomas93@gmail.com>
2024-05-22 15:51:37 +0200

    fix: remove type undefined

--- apps/client/instamint-webapp/src/types/hooks/users/profileUser.types.ts
+++ apps/client/instamint-webapp/src/types/hooks/users/profileUser.types.ts
@@ -1,15 +1,13 @@
 import type { Publication } from "../publications/publications.types"
 
 export type ProfileUser = {
-  result:
-    | {
-        email: string
-        username: string
-        bio: string | null
-        avatar: string | null
-        publicationData: Publication[]
-      }
-    | undefined
+  result: {
+    email: string
+    username: string
+    bio: string | null
+    avatar: string | null
+    publicationData: Publication[]
+  }
   followers: {
     count: number
   }
